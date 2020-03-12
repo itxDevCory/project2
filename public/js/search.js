b79d195b0018dc720dcb71da85ab90a9dcff7675
Anya Laudenslager <anyalaudenslager@Anyas-MacBook-Pro.local>
2020-03-12 16:00:32 -0400

    added to .js and html-routes

--- public/js/search.js
+++ public/js/search.js
@@ -1,7 +1,13 @@
+//Gets input from the user in index.html and retrieves data from api-routes
 $(document).ready(function() {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
+  $.get("/api/user/myID").then(function() {
+    $.get("api/search", {
+      email: email,
+      password: password,
+      zipcode: zipcode,
+      start: start,
+      end: end,
+      type: type
+    });
   });
 });
